c004720604ef0f21f8b188ac2aced90c207a8b75
PaleRoom <PushistiiYashikWr@Yandex.ru>
2022-02-23 17:50:10 +0500

    Initial commit

---
+++ src/PUSH_P/Calculator.java
@@ -0,0 +1,29 @@
+package PUSH_P;
+
+import java.util.function.*;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    // Не была учтена вероятность деления на 0. Добавил генерирование исключения
+    BinaryOperator<Integer> devide = (x, y) -> {
+        try {
+            if (y == 0)
+                throw new ArithmeticException("Exception: divide by zero.");
+            return x / y;
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    };
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+}
