03d86917857f1704498de557c4365511da246c2f
PaleRoom <PushistiiYashikWr@Yandex.ru>
2022-02-28 21:43:06 +0500

    Исправление Devide с тернарным оператором

--- src/PUSH_P/Calculator.java
+++ src/PUSH_P/Calculator.java
@@ -9,17 +9,9 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    // Не была учтена вероятность деления на 0. Добавил генерирование исключения
-    BinaryOperator<Integer> devide = (x, y) -> {
-        try {
-            if (y == 0)
-                throw new ArithmeticException("Exception: divide by zero.");
-            return x / y;
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-            return 0;
-        }
-    };
+    // Не была учтена вероятность деления на 0.
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? 0 : x / y;
+
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
